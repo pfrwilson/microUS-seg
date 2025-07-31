f8461881a5434de9a292175d6929639c0229f8f7
Paul Wilson <78878058+pfrwilson@users.noreply.github.com>
2025-07-31 14:01:10 -0400

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,9 +1,17 @@
+import torch
+
 dependencies = ['torch', 'torchvision', 'PIL', 'monai']
 
 
 def medsam_unetr(pretrained=True, **kwargs):
     from src.unetr import medsam_unetr
     model = medsam_unetr()
+
+    if pretrained: 
+        state_dict = torch.hub.load_state_dict_from_url(
+            'https://github.com/pfrwilson/microUS-seg/releases/download/v0.0.0/medsam_unetr_microsegnet.pth', map_location='cpu'
+        )
+        print(model.load_state_dict(state_dict))
 
     return model
     
