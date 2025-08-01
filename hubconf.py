f82141338a5dce84ec2097d23250d871bbca650c
Paul F R Wilson <pfrwilson@gmail.com>
2025-08-01 15:00:51 -0400

    add preprocessing config

--- hubconf.py
+++ hubconf.py
@@ -1,18 +1,26 @@
 import torch
 
-dependencies = ['torch', 'torchvision', 'PIL', 'monai']
+dependencies = ["torch", "torchvision", "PIL", "monai"]
 
 
 def medsam_unetr(pretrained=True, **kwargs):
     from src.unetr import medsam_unetr
+
     model = medsam_unetr()
 
-    if pretrained: 
+    if pretrained:
         state_dict = torch.hub.load_state_dict_from_url(
-            'https://github.com/pfrwilson/microUS-seg/releases/download/v0.0.0/medsam_unetr_microsegnet.pth', map_location='cpu'
+            "https://github.com/pfrwilson/microUS-seg/releases/download/v0.0.0/medsam_unetr_microsegnet.pth",
+            map_location="cpu",
         )
         print(model.load_state_dict(state_dict))
 
+        model.preprocessing_cfg = dict(
+            mean=[0, 0, 0],
+            std=[1, 1, 1],
+            in_channels=3,
+            in_shape=[224, 224],
+            resample_mode="bilinear",
+        )
+
     return model
-    
-     
